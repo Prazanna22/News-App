df2c44aff0f01e5ef5d2a97d4071cabfa376d1e0
Durga Prasanna <prasanna.guru02@gmail.com>
2024-10-24 15:34:46 +0530

    finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchArticles } from "../store/newSlice";
 import { AppDispatch } from "../store/store";
-import like from '../assets/like.png';
 import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
